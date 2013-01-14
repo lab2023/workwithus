e2e34bdc42a01b31514a88f3084b057ef90c6239
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2013-01-14 19:38:16 +0200

    Update pull_requests.rb

--- pull_requests.rb
+++ pull_requests.rb
@@ -1,12 +1,10 @@
 # Fixed these bugs and pull request
 
 # 1. Bug
-
 number = 2
 if ( number = 2 ) {           ## (2 == number)
   put 'Number value is two'
 }
 
 # 2. Bug
-
 12.reverse                    ## 12.to_s.reverse
