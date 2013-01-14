2d0feab66be8347c60b83735412f3c991442b5ef
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2013-01-14 19:17:48 +0200

    Update pull_requests.rb

--- pull_requests.rb
+++ pull_requests.rb
@@ -3,10 +3,10 @@
 # 1. Bug
 
 number = 2
-if ( number = 2 ) {           ## Tip 2 == number
+if ( number = 2 ) {           ## (2 == number)
   put 'Number value is two'
 }
 
 # 2. Bug
 
-12.reverse                    ## Tip 12.to_s.reverse
+12.reverse                    ## 12.to_s.reverse
