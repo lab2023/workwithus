d82a54031936f175431b837f1ac9022a58771435
Berkhan Berkdemir <24360355+BerkhanBerkdemir@users.noreply.github.com>
2019-02-26 11:18:52 -0800

    :bug: fix multiple syntax errors
    * Fix wrong usage of curly braces
    * Add to_s for reverse method since int can't be reversed
    * Use guard clause instead of block if statement

--- pull_requests.rb
+++ pull_requests.rb
@@ -1,10 +1,5 @@
-# Fixed these bugs and pull request
-
-# 1. Bug
 number = 2
-if ( number = 2 ) {           ## (2 == number)
-  put 'Number value is two'
-}
 
-# 2. Bug
-12.reverse                    ## 12.to_s.reverse
+puts 'Number value is two' if number == 2
+
+12.to_s.reverse
